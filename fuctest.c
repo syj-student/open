26b8f4bc116a4ea29da6a4e67a27daa433992e28
yusong <42.4.yusong@gmail.com>
2021-08-22 16:09:23 +0900

    test

--- fuctest.c
+++ fuctest.c
@@ -12,52 +12,14 @@
            void *: "pointer to void",                int *: "pointer to int",         \
           default: "other")
 
-void mergeSort(int data[], int *ret, int p, int r);
-void merge(int data[], int *ret, int p, int q, int r);
-void    testprint(int *test)
+
+int main(void)
 {
-    for (int i = 0; i < 10; i++)
-        printf("%d ", test[i]);
-    printf("\n");
-}
-void    statictestprint(int *test)
-{
-    testprint(test);
-}
-void    statictest(int i, int j, const int len)
-{
-    const int tmp;
-    tmp = len;
-    static int test[tmp];
-    if (i == 10)
-    {
-        statictestprint(test);
-        return;
-    }
-    test[i] = j;
-    testprint(test);
-    printf("-----------------\n");
-    statictest(++i, ++j, len);
-}
-int main() {
-    statictest(0, -1, 10);
-}
-void mergeSort(int data[], int *ret, int p, int r) {
-    int q;
-    if(p<r) {
-        q = (p+r)/2;
-        mergeSort(data, ret, p , q);
-        mergeSort(data, ret, q+1, r);
-        merge(data, ret, p, q, r);
-    }
-}
-void merge(int data[], int *tmp, int p, int q, int r) {
-    int i = p, j = q+1, k = p;
-    while(i<=q && j<=r) {
-        if(data[i] <= data[j]) tmp[k++] = data[i++];
-        else tmp[k++] = data[j++];
-    }
-    while(i<=q) tmp[k++] = data[i++];
-    while(j<=r) tmp[k++] = data[j++];
-    for(int a = p; a<=r; a++) data[a] = tmp[a];
+    char *tmp = "Hello Wolrd\n";
+    char tmp2[] = "Hello World\n";
+
+    printf("%s%s", tmp, tmp2);
+    tmp2[0] = 'a';
+    printf("%s%s", typename(tmp), typename(tmp2));
+    return (0);
 }
